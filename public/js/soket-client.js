ccbcb2784e1f19641dbda7f5bfa4b3860c9036a9
pablogimenez <pablo.gimenez.zorrilla@gmail.com>
2023-03-12 12:05:11 -0300

    first commit

---
+++ public/js/soket-client.js
@@ -0,0 +1,36 @@
+// Referencias de HTML
+const lblOnline  = document.querySelector('#lblOnline')
+const lblOffline = document.querySelector('#lblOffline')
+const txtMensaje = document.querySelector('#txtMensaje')
+const btnEnviar  = document.querySelector('#btnEnviar')
+
+const socket = io();
+
+socket.on('connect', () => {
+  lblOnline.style.display = ''
+  lblOffline.style.display = 'none'
+})
+
+socket.on('disconnect', () => {
+  lblOffline.style.display = ''
+  lblOnline.style.display = 'none'
+})
+
+socket.on('enviar-mensaje', payload => {
+  console.log(payload)
+})
+
+btnEnviar.addEventListener('click', () => {
+  const mensaje = txtMensaje.value;
+
+  const payload = {
+    mensaje,
+    id: '12345678',
+    fecha: new Date().getTime()
+  }
+  
+  socket.emit('enviar-mensaje', payload, ( id ) => {
+    console.log(`Desde el server ${id}`)
+  })
+
+})
